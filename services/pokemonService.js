d32c7d0459bb1263464bb5f54609a80f0fcba81e
LongMyNgoc <longmyngoc2004@gmail.com>
2024-10-23 14:14:47 +0700

    Insert Gen

--- services/pokemonService.js
+++ services/pokemonService.js
@@ -6,12 +6,17 @@ export const fetchPokemonDataByBatch = async (offset, limit) => {
         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
         const detailedPokemon = await Promise.all(
             response.data.results.map(async (pokemon, index) => {
+                // Fetch thông tin chi tiết về Pokémon
                 const pokemonData = await axios.get(pokemon.url);
+                
+                // Fetch thông tin về thế hệ từ pokemon-species endpoint
+                const speciesData = await axios.get(pokemonData.data.species.url);
+                
                 return {
                     name: pokemonData.data.name,
                     sprite: pokemonData.data.sprites.front_default,
                     types: pokemonData.data.types.map(type => type.type.name),
-                    generation: 'Gen 1', // Bạn có thể thay đổi logic này nếu cần
+                    generation: speciesData.data.generation.name, // Lấy thế hệ từ species data
                     number: offset + index + 1, // Tính toán số thứ tự dựa trên offset
                 };
             })
@@ -21,16 +26,4 @@ export const fetchPokemonDataByBatch = async (offset, limit) => {
         console.error('Error fetching data from PokeAPI:', error);
         throw new Error('Failed to fetch Pokémon data from PokeAPI');
     }
-};
-
-// Hàm để fetch toàn bộ dữ liệu Pokémon
-export const fetchAllPokemonData = async () => {
-    let allPokemon = [];
-    const limit = 205; // Fetch theo từng lô 205 Pokémon
-    const total = 1025; // Tổng số Pokémon
-    for (let offset = 0; offset < total; offset += limit) {
-        const pokemonBatch = await fetchPokemonDataByBatch(offset, limit);
-        allPokemon = [...allPokemon, ...pokemonBatch];
-    }
-    return allPokemon;
 };
